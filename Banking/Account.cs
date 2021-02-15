37c8fa3445d727dcfb76695908d2ef169d4d61aa
Jada <jada.fagan@gmail.com>
2021-02-15 17:44:03 -0500

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class Account
+    {
+
+        public string AccountNumber { get; set; }
+        public string RoutingNumber { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdrawl(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public Account() { }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new Account()
+            {
+                AccountNumber = "00001",
+                RoutingNumber = "12356789",
+                Description = "Craft Fund",
+            };
+
+            Console.WriteLine($" The {account1.Description} account has a ${account1.Balance} balance");
+            account1.Deposit(750000);
+            Console.WriteLine($" The {account1.Description} account has a ${account1.Balance} balance");
+            account1.Withdrawl(5000);
+            Console.WriteLine($" The {account1.Description} account has a ${account1.Balance} balance");
+
+        }
+    }
+}
